3d93623839bb78ea34fbdfbacffd70a2583ab2ad
[preedomly] <[preedomly@163.com]>
2018-05-07 18:17:56 +0800

    12

--- sekouApp/src/models/zncx.js
+++ sekouApp/src/models/zncx.js
@@ -1,5 +1,6 @@
 import store from "../index";
 import { routerRedux } from "dva/router";
+import { publish } from "../core/arbiter";
 
 const { dispatch } = store;
 
@@ -8,63 +9,42 @@ export default {
   state: {
     tabs: [
       {
-        title: "船舶信息",
-        type: "cbxx",
-        tablename : 'V_IMAP_SCCT_BERTH',
-        icon: require('../images/zncx/船舶信息.svg'),
-        cols: {imo: "IMO号",shc: "商业航次号",cm: '船名', mhc: "码头航次号", mt: "靠泊码头"},
-        tip: {
-            fld: 'zt', 
-            disp: [
-                {iconStyle: {background: '#1fd781'}, tip:'离港', tipStyle: {color: '#1fd781'}},
-                {iconStyle: {background: '#1890ff'}, tip:'靠泊', tipStyle: {color: '#1890ff'}},
-            ]
-        },
-        datas: [
-            {imo: "QWE12345", shc: "B-SZ20170422",cm: '航海号1', mhc: "SFG100023", mt: "西海码头", zt: 0},
-            {imo: "ASD123000300", shc: "B-SZ20170422",cm: '航海号12', mhc: "SFG100023", mt: "一湾码头", zt: 1},
-            {imo: "ASD123000300", shc: "B-SZ20170422",cm: '航海号I', mhc: "SFG100023", mt: "西海码头", zt: 0},
-            {imo: "ASD123000300", shc: "B-SZ20170422",cm: '航海号12', mhc: "SFG100023", mt: "一湾码头", zt: 1},
-            {imo: "ASD123000300", shc: "B-SZ20170422",cm: '航海号12', mhc: "SFG100023", mt: "一湾码头", zt: 1},
-        ]
+        title: "船舶信息", type: "cbxx", tablename: 'V_IMAP_SCCT_BERTH', icon: require('../images/zncx/船舶信息.svg'),
       },
       {
-        title: "提单信息",
-        type: "tdxx",
-        tablename : '',
-        icon: require('../images/zncx/提单信息.svg'),
-        cols: {imo: "IMO号",shc: "商业航次号",cm: '船名', mhc: "码头航次号", tdh: "靠泊码头"},
-        datas: [
-            {imo: "QWE12345", shc: "B-SZ20170422",cm: '航海号1', mhc: "SFG100023", tdh: "RT-13344899"},
-            {imo: "ASD123000300", shc: "B-SZ20170422",cm: '航海号12', mhc: "SFG100023", tdh: "RT-QW5673"},
-        ]
+        title: "提单信息", type: "tdxx", tablename: '', icon: require('../images/zncx/提单信息.svg'),
       },
       {
-        title: "集装箱信息",
-        type: "jzxxx",
-        tablename : 'V_IMAP_SCCT_ONYARD',
-        icon: require('../images/zncx/集装箱信息.svg'),
-        cols: {imo: "IMO号",shc: "商业航次号",cm: '船名', mhc: "码头航次号", zymt: "作业码头"},
-        datas: [
-            {imo: "QWE12345", shc: "B-SZ20170422",cm: '航海号1', mhc: "SFG100023", zymt: "西海码头"},
-            {imo: "ASD123000300", shc: "B-SZ20170422",cm: '航海号12', mhc: "SFG100023", zymtzt: "一湾码头"},
-        ]
+        title: "集装箱信息", type: "jzxxx",  tablename: 'V_IMAP_SCCT_ONYARD', icon: require('../images/zncx/集装箱信息.svg'),
       },
       {
-        title: "车辆信息",
-        type: "clxx",
-        tablename : '',
-        icon: require('../images/zncx/车辆信息.svg'),
-        cols: {cph: "车牌号", bm: "拖车海关编号", mc: "企业名称",pm: '车辆牌名', no: "车身号码"},
-        datas: [
-            {cph: "粤BL2345", bm: "B-S20170422", mc: "上海一汽", pm: "HJ123456", no: "W123456"},
-            {cph: "粤BA2345", bm: "B-S20170422", mc: "上海一汽", pm: "HJ123456", no: "W123456"},
-        ]
+        title: "车辆信息", type: "clxx", tablename: '', icon: require('../images/zncx/车辆信息.svg'),
       }
     ],
     itemClick: (obj, lx, idx) => {
-        dispatch(routerRedux.push("/zncx_xx/" + lx + "/1"));
+      dispatch(routerRedux.push("/zncx_xx/" + lx + "/1"));
+    },
+    list: [],
+  },
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      let datas = [];
+      datas.splice(0,datas.length);
+      let responseCXJG = yield ( publish('getData', { svn: 'skhg_loader', tableName: payload.tablename,  data: { pageno: payload.count, pagesize: 10, where: payload.value ? "TERMINALCODE like '%" + payload.value + "'" : "1=1" } }));
+      let responseCXBM = yield ( publish('tableName_find') );
+      responseCXJG[0].features.map(x => datas.push(x.attributes));
+      yield put({
+        type: 'appendList',
+        payload: [datas, responseCXBM] ? [datas, responseCXBM] : [],
+      });
     }
   },
-  reducers: {}
+  reducers: {
+    appendList(state, action) {
+      return {
+        ...state,
+        list: action.payload,
+      };
+    },
+  }
 };
--- sekouApp/src/routes/zncx/App.js
+++ sekouApp/src/routes/zncx/App.js
@@ -7,250 +7,146 @@ import $ from 'jquery';
 import "./app.less";
 import "./action"
 
-
-class Demo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
-            refreshing: true,
-            isLoading: true,
-            height: document.documentElement.clientHeight,
-            useBodyScroll: false,
-            names: [],
-            datas: [],
-            loading: true,
-            index: 1,       //上拉加载数据
-            value: null,   //港口选择
-            tablename: null, //查询的表名
-        };
-    }
-
-
-    componentDidMount() {
-        const hei = this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop;
-        this.fecthData();
-        setTimeout(() => {
-            this.resa = this.state.datas;
-            this.numss = true;
-            this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(this.resa),
-                height: hei,
-                refreshing: false,
-                isLoading: false,
-            });
-        }, 1500);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        console.log(nextProps)
-        if (nextProps.val !== null) {
-            this.setState({ value: nextProps.val, index: 1, tablename: nextProps.tablename }, () => this.fecthData());
-            setTimeout(() => {
-                this.resa = this.state.datas;
-                this.numss = true;
-                this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(this.resa),
-                    refreshing: false,
-                    isLoading: false,
-                });
-            }, 1500);
-        } else {
-            this.setState({ index: 1, tablename: nextProps.tablename }, () => this.fecthData());
-            setTimeout(() => {
-                this.resa = this.state.datas;
-                this.numss = true;
-                this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(this.resa),
-                    refreshing: false,
-                    isLoading: false,
-                });
-            }, 1500);
-        }
-    }
-    componentDidUpdate() {
-        if (this.state.useBodyScroll) {
-            document.body.style.overflow = 'auto';
-        } else {
-            document.body.style.overflow = 'hidden';
-        }
-    }
-
-    fecthData() {
-        console.log(this.props.tablename);
-        const hei = this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop;
-        Toast.hide();
-        //查询数据请求，并且用cloneWithRows处理数据
-        Promise.all([
-            publish('getData', { svn: 'skhg_loader', tableName: this.state.tablename, data: { pageno: this.state.index, pagesize: 5, where: this.state.value ? "TERMINALCODE like '%" + this.state.value + "'" : "1=1" } }),
-            publish('tableName_find')
-        ]).then(res => {
-            if (res[0][0].features.length > 0) {
-                this.numss = true;
-                let datas = [];
-                res[0][0].features.map(x => datas.push(x.attributes));
-                this.setState({
-                    datas: datas,
-                    names: res[1][0].features[0].table
-                })
-            } else {
-                this.numss = false;
-                Toast.fail('无更多的数据！', 1)
-            }
-        }).catch(err => {
-            console.log(err)
-        });
-
-    }
-
-    onRefresh = () => {
-        this.setState({ refreshing: true, isLoading: true, index: 1 }, () => this.fecthData());
-        setTimeout(() => {
-            this.resa = this.state.datas;
-            this.setState({
-                dataSource: this.state.dataSource.cloneWithRows(this.resa),
-                refreshing: false,
-                isLoading: false,
-            });
-        }, 5000);
-    };
-
-    onEndReached = (event) => {
-        this.setState({ index: this.state.index + 1 }, () => this.fecthData());
-        setTimeout(() => {
-            if (this.numss) {
-                this.resa = [...this.resa, ...this.state.datas];
-                this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(this.resa),
-                    isLoading: true,
-                });
-            } else {
-                this.setState({ isLoading: false, });
-            }
-        }, 1000);
-    }
-
-    render() {
-        const separator = (sectionID, rowID) => (<div key={`${sectionID}-${rowID}`} style={{ backgroundColor: '#F5F5F9', height: 8, borderTop: '1px solid #ECECED', borderBottom: '1px solid #ECECED', }} />);
-        let { icon, type } = this.props;
-        let { datas, names } = this.state;
-        let index = this.state.datas.length - 1;
-        const row = (rowData, sectionID, rowID) => {
-            if (index < 0) index = this.state.datas.length - 1;
-            const obj = this.state.datas[index--];
-            return (
-                <div key={rowID} style={{ padding: '0 15px', backgroundColor: 'white', }}>
-                    <table key={rowID} className="zncx_table" >
-                        <tbody>
-                            {
-                                this.state.datas.length > 1 ? Object.keys(obj).map((key, id) => {
-                                    if (id + 0 && id < 7 && names[0][key]) {
-                                        return (
-                                            <tr key={id + type} style={{ color: "#8e8e8e" }}>
-                                                <td></td>
-                                                <td style={{ width: '50%' }}>{names ? names[0][key] : null}:</td>
-                                                <td>{obj[key]}</td>
-                                                <td></td>
-                                            </tr>
-                                        )
-                                    }
-                                    if (id < 1) {
-                                        return (
-                                            <tr key={id + type} style={{ color: "#1890ff" }}>
-                                                <td className="zncx_table_col_1"><div className="zncx_table_img"><img src={icon} alt="" /></div></td>
-                                                <td>{names ? names[0][key] : null}:</td>
-                                                <td>{obj[key]}</td>
-                                                <td></td>
-                                            </tr>
-                                        );
-                                    }
-                                }) : null
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            );
-        };
-        return (<div>
-            <ListView
-                key={this.state.useBodyScroll}
-                ref={el => this.lv = el}
-                dataSource={this.state.dataSource}
-                renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
-                    {this.state.isLoading ? 'Loading...' : 'Loaded'}
-                </div>)}
-                renderRow={row}
-                renderSeparator={separator}
-                useBodyScroll={this.state.useBodyScroll}
-                style={{
-                    height: this.state.height,
-                    border: '1px solid #ddd',
-                    margin: '5px 0',
-                }}
-                pullToRefresh={<PullToRefresh
-                    refreshing={this.state.refreshing}
-                    onRefresh={this.onRefresh}
-                />}
-                onEndReached={this.onEndReached}
-                pageSize={5}
-            />
-        </div>);
-    }
-}
-
-export default connect(({ zncx, loading }) => ({ tabs: zncx.tabs, itemClick: zncx.itemClick }))(
+export default connect(({ zncx, loading }) => ({ ...zncx }))(
     class Mtcb extends Component {
         constructor(props) {
             super(props);
             this.state = {
-                value: null,
-                index: 0,
-                tabo: [],
-                datas: [],
-                loading: true,
-                data: [],
-                dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+                seasonsValue: null,
+                pageSize: 0,
+                dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
+                refreshing: true,
+                isLoading: true,        //是否显示加载
+                height: document.documentElement.clientHeight,
+                useBodyScroll: false,
+                PageDisplayDate: [],   //页面展示数据
+                PageTitleDate: {},     //页面标题数据
+                count: 2,            //翻页
+                tableName: 'V_IMAP_SCCT_BERTH', //表名
+
             }
         }
 
         componentDidMount() {
-            if (this.state.index === 0) {
-                this.setState({ index: 1 });
+            this.setState({ pageSize: 0 });
+            this.fecthData();
+        };
 
-            }
-        }
 
-        //单列样式
-        _renderRow(row, sectionId, rowId) {
+        /** 若上拉加载数据，高度则添加下拉框 ？ */
+        // componentDidUpdate() {
+        //     if (this.state.useBodyScroll) {
+        //         document.body.style.overflow = 'auto';
+        //     } else {
+        //         document.body.style.overflow = 'hidden';
+        //     }
+        // }
 
-        }
-
+        /** 切换标题签 */
         handefind = (tab, index) => {
-            this.setState({ index, value: null });
-            // window.localStorage.setItem('mthzx_tabs_indx', index)
+            this.setState({ pageSize: index, value: null, PageDisplayDate: [], PageTitleDate: {} },() => this.fecthData());
+            
         }
 
-        handeChange = (tab, index) => {
-            this.setState({ index, value: null });
-        }
 
-        onChange = (value) => {
-            this.setState({
-                value,
+        onRefresh = () => {
+            console.log("下拉一次");
+            this.setState({ count: 2 },() => this.fecthData());
+        };
+
+        /** 上拉事件 */
+        onEndReached = (event) => {
+            console.log("上拉一次");
+            this.props.dispatch({
+                type: 'zncx/fetch',
+                payload: {
+                    tablename: this.props.tabs[this.state.pageSize].tablename,
+                    count: this.state.count,
+                },
             });
+            this.setState({ count: this.state.count + 1 }, () => setTimeout(() => {
+                if (this.props.list[0].length > 0) {
+                    this.resa = [...this.resa, ...this.props.list[0]];
+                    this.setState({
+                        dataSource: this.state.dataSource.cloneWithRows(this.resa),
+                        isLoading: true,
+                        PageDisplayDate: this.props.list[0],
+                    });
+                } else {
+                    Toast.fail('没有多余的数据了！', 1);
+                    this.setState({ isLoading: false, });
+                }
+            }, 1000));
         }
-        onScrollChange = (value) => {
+        /** 回滚 */
+        fecthData() {
+            this.resa = {};
+            const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
+            this.props.dispatch({
+                type: 'zncx/fetch',
+                payload: {
+                    tablename: this.props.tabs[this.state.pageSize].tablename,
+                    count: 1,
+                },
+            }, setTimeout(() => {
+                this.resa = this.props.list[0];         //支持旧数据
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(this.resa),        //分类
+                    height: hei,
+                    refreshing: false,
+                    isLoading: true,
+                    PageDisplayDate: this.props.list[0],
+                    PageTitleDate: this.props.list[1][0][this.props.tabs[this.state.pageSize].type]
+                });
+            }, 1000));
         }
-
         render() {
-            let { tabs, itemClick = () => { } } = this.props;
-            let { index, datas, tabo = {} } = this.state;
-            const seasons = [[{ label: 'SCT', value: 'SCT', }, { label: 'CCT', value: 'CCT', }, { label: 'MCT', value: 'MCT', }],];
             let items = [];
-            if (index === 2) {
+            let { tabs, itemClick = () => { }, list = [] } = this.props;
+            let { PageDisplayDate, PageTitleDate, pageSize } = this.state;
+            let index = PageDisplayDate.length - 1;
+            const separator = (sectionID, rowID) => (<div key={`${sectionID}-${rowID}`} style={{ backgroundColor: '#F5F5F9', height: 8, borderTop: '1px solid #ECECED', borderBottom: '1px solid #ECECED', }} />);
+            const seasons = [[{ label: 'SCT', value: 'SCT', }, { label: 'CCT', value: 'CCT', }, { label: 'MCT', value: 'MCT', }]];
+            const row = (rowData, sectionID, rowID) => {
+                if (index < 0) index = PageDisplayDate.length - 1;
+                const obj = PageDisplayDate[index--];
+                return (
+                    <div key={rowID} style={{ padding: '0 15px', backgroundColor: 'white', }}>
+                        <table key={rowID} className="zncx_table" >
+                            <tbody>
+                                {
+                                    Object.keys(PageTitleDate).map((key, id) => {
+                                        if (id + 0 && id < 5 && PageTitleDate) {
+                                            return (
+                                                <tr key={id + key} style={{ color: "#8e8e8e" }}>
+                                                    <td></td>
+                                                    <td style={{ width: '50%' }}>{PageTitleDate[key]}:</td>
+                                                    <td>{obj[key]}</td>
+                                                    <td></td>
+                                                </tr>
+                                            )
+                                        }
+                                        if (id < 1) {
+                                            return (
+                                                <tr key={id + key} style={{ color: "#1890ff" }}>
+                                                    <td className="zncx_table_col_1"><div className="zncx_table_img"><img src={tabs[this.state.pageSize].icon} alt="" /></div></td>
+                                                    <td>{PageTitleDate[key]}:</td>
+                                                    <td>{obj[key]}</td>
+                                                    <td></td>
+                                                </tr>
+                                            );
+                                        }
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                );
+            };
+            if (pageSize === 2) {
                 items = [
                     <div key="valChange">
-                        <Picker data={seasons} cascade={false} title="选择港口" extra="请选择(可选)" value={this.state.value} onChange={v => this.setState({ value: v })}>
+                        <Picker data={seasons} cascade={false} title="选择港口" extra="请选择(可选)" value={this.state.seasonsValue} onChange={v => this.setState({ seasonsValue: v })}>
                             <List.Item arrow="horizontal">港口选择</List.Item>
                         </Picker>
                     </div>
@@ -260,19 +156,32 @@ export default connect(({ zncx, loading }) => ({ tabs: zncx.tabs, itemClick: znc
                 <Tabs
                     tabs={tabs.map(tab => ({ title: tab.title }))}
                     swipeable={true}
-                    onChange={(tab, index, type) => this.handeChange(tab, index, type)}
+                    initialPage={0}
+                    prerenderingSiblingsNumber={0}
+                    onChange={null}
                     onTabClick={(tab, index, type) => this.handefind(tab, index, type)}
                 >
                     {tabs.map((tab, idsx) => {
-                        let { cols = {}, icon, tip, type, tablename } = tab;
-                        let keys = Object.keys(tabo);
+                        let { icon, tip, type, tablename } = tab;
                         return (
                             <div key={idsx} className="zncx">
                                 <div className="zncx_bar">
                                     <SearchBar placeholder="请输入" onSubmit={value => this.handefind(value)} ref={ref => this.autoFocusInst = ref} />
                                 </div>
                                 {items}
-                                <Demo icon={icon} type={type} index={index} tablename={tablename} val={idsx === index ? this.state.value : null} />
+                                <ListView
+                                    key={this.state.useBodyScroll}
+                                    ref={el => this.lv = el}
+                                    dataSource={this.state.dataSource}
+                                    renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}> {this.state.isLoading ? '加载中...' : '暂无更多数据'} </div>)}
+                                    renderRow={row}
+                                    renderSeparator={separator}
+                                    useBodyScroll={this.state.useBodyScroll}
+                                    style={{ height: this.state.height, border: '1px solid #ddd', margin: '5px 0', }}
+                                    pullToRefresh={<PullToRefresh refreshing={this.state.refreshing} onRefresh={this.onRefresh} />}
+                                    onEndReached={this.onEndReached}
+                                    pageSize={5}
+                                />
                                 <div />
                             </div>
                         );
